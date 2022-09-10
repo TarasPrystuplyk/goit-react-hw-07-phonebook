3208906fd3d33353309203aaaf4372633f16d8d9
Taras Prystuplyk <1988taras@ukr.net>
2022-09-10 17:05:07 +0300

    fix rtk

---
+++ src/components/list/ContactsListItem.jsx
@@ -0,0 +1,17 @@
+// import PropTypes from 'prop-types';
+// import { ContactsItem, DeleteButton, Text } from './ContactsItemStyled';
+// export const ContactsListItem = ({ item, deleteContact }) => {
+//   return (
+//     <ContactsItem>
+//       <Text>Name: {item.name}</Text>
+//       <Text>Number: {item.number}</Text>
+//       <DeleteButton type="button" onClick={() => deleteContact(item.id)}>
+//         Delete
+//       </DeleteButton>
+//     </ContactsItem>
+//   );
+// };
+// ContactsListItem.propTypes = {
+//   deleteContact: PropTypes.func,
+//   item: PropTypes.objectOf(PropTypes.string),
+// };
